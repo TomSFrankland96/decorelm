c2ec66f250b00a34f1c9e87b17ce8cdfa4190324
Tom Frankland <tom.frankland@aol.com>
2022-02-28 16:49:52 +0000

    change text on enquiry page

--- src/views/pages/enquire/Main.js
+++ src/views/pages/enquire/Main.js
@@ -19,7 +19,7 @@ export default class Main extends Component {
                 <Container>
                     <Row>
                         <div className="mx-auto enquire-text form">
-                            <p>Got a project that you'd need help with? Book a free, no-obligation discovery call to discuss your design ambition and receive your final quote for the project.</p>
+                            <p>Got a project that you need help with? Book a free, no-obligation discovery call to discuss your design ambition and learn the next steps.</p>
                             <a href="https://calendly.com/decorelm/15min" target="_blank" style={{ color: "#494949", textDecoration: "none" }}><Button className="my-5 book-call">book a free call <i class="bi bi-arrow-right"></i></Button></a>
                         </div>
                         <div className="mx-auto enquire-text form">
