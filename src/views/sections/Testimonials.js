a07695b3944d10f8f498a305e0bf248179968b88
Tom Frankland <tom.frankland@aol.com>
2022-02-28 22:13:08 +0000

    fixing bark image for testimonials

--- src/views/sections/Testimonials.js
+++ src/views/sections/Testimonials.js
@@ -22,7 +22,7 @@ export default class Testimonial extends Component {
                                 <TestimonialsCarousel />
                             </Row>
                             <div className="d-flex justify-content-center my-2">
-                                <a href="https://www.bark.com/en/gb/company/decorelm-design-studio/RnVay/" target="_blank"><img className="me-3" src="https://res.cloudinary.com/decorelm/image/upload/v1643704501/decorelm-website/coe-badge-2021_ulgmqw.png" alt="Kerry Armstrong in Leeds, West Yorkshire, UK on Bark" style={{width: "75px"}}/></a>
+                                <a href="https://www.bark.com/en/gb/company/decorelm-design-studio/RnVay/" target="_blank"><img className="me-3" src="https://res.cloudinary.com/decorelm/image/upload/v1646085413/services/Bark_g0oqrg.png" alt="Kerry Armstrong in Leeds, West Yorkshire, UK on Bark" style={{width: "75px"}}/></a>
                                 <a href="https://www.houzz.co.uk/pro/decorelm/decorelm" target="_blank"><img src="https://st.hzcdn.com/static_en-GB/badge_57_8@2x.png" alt="Kerry Armstrong in Leeds, West Yorkshire, UK on Houzz" border="0" style={{width: "55px"}} /></a>
                             </div>
                             <div className="d-flex justify-content-center mt-3">
