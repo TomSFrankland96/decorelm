eaae6d82688519a2aeea4c82ac1934b9249cc441
Tom Frankland <tom.frankland@aol.com>
2022-03-21 12:41:13 +0000

    fixed vh for text on enquire page

--- src/views/pages/enquire/Main.js
+++ src/views/pages/enquire/Main.js
@@ -18,11 +18,11 @@ export default class Main extends Component {
             <section>
                 <Container>
                     <Row>
-                        <div className="mx-auto enquire-text form">
+                        <div className="mx-auto enquire-text">
                             <p>Got a project that you need help with? Book a free, no-obligation discovery call to discuss your design ambition and learn the next steps.</p>
                             <a href="https://calendly.com/decorelm/15min" target="_blank" style={{ color: "#494949", textDecoration: "none" }}><Button className="my-5 book-call">book a free call <i class="bi bi-arrow-right"></i></Button></a>
                         </div>
-                        <div className="mx-auto enquire-text form">
+                        <div className="mx-auto enquire-text">
                             <p>Alternatively, if you'd like to make an email enquiry or ask any questions please complete the form below.</p>
                         </div>
                         <EnquireForm />
